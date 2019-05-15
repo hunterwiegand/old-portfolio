9dbde2e3cd21670f1e40359b872cd386a7713f82
Hunter Wiegand <hunter_wiegand@yahoo.com>
2019-05-15 02:13:21 -0700

    added content for FFT

--- scripts/magic.js
+++ scripts/magic.js
@@ -31,13 +31,13 @@ function getContext(id) {
         case "blobber-readme":
             return BlobberReadme.format;
         case "fft-readme":
-            return FFtReadme.format;
+            return FFTReadme.format;
         case "mystic-spells-readme":
-            return mysticSpellsReadme;
+            return MysticSpellsReadme.format;
         case "node-siri-readme":
-            return nodeSiriReadme;
+            return NodeSiriReadme.format;
         case "giffy-readme":
-            return giffyReadme;
+            return GiffyReadme.format;
 
     }
 }
@@ -70,25 +70,74 @@ const BlobberReadme = {
 const FFTReadme = {
     title: $("<span>").html("Food For Thought"),
     tech0: [
-        ""
+        "HTML5",
+        "Bootstrap",
+        "FireBase",
+        "Git version control"
     ],
     tech1: [
-        ""
+        "fullCalendar.js",
+        "jQuery",
+        "EdamameAPI",
+        "ajax"
     ],
 
-    insight: $("<span>").html(""),
+    insight: $("<span>").html("Food For Thought was my first project working with a team. During this project I learned the basics of Firebase, and how to handle state change. I also dove into unknown waters while figuring out how to utilize the fullCalendar.js library. I have many thanks to this project for teaching me the importance of Git version control, and how to troubleshoot when gitHub doesn't play nice."),
 
-    description: $("<span>").html(""),
+    description: $("<span>").html("Food For Thought was created to solve an issue we all get faced with. 'I want to cook all my left over ingredients, but I have no idea what to make with them'. Well fret no more, because with Food For Thought all you have to do is enter your pantry items, then head over to the recipe page; select the items you want to cook with, and BAM. You are greeted with three recipes to choose from. But that's not all! After you decide you want to cook the meal, you can plan it out and choose what day you want to eat it on. This process will also populate your shopping cart with any items you're missing from the recipe"),
 
     format: function() {
         return formatReadme(FFTReadme.title.html(), FFTReadme.tech0, FFTReadme.tech1, FFTReadme.insight.html(), FFTReadme.description.html())
     }
 }
 
-function formatReadme(title, tech0, tech1, insight, description) {
-    console.log(title);
-    const readmeTemplate = $("<div>").addClass("container");
+const MysticSpellsReadme = {
+    title: $("<span>").html("Food For Thought"),
+    tech0: [
+        ""
+    ],
+    tech1: [
+        ""
+    ],
+    insight: $("<span>").html(""),
+    description: $("<span>").html(""),
+    format: function() {
+        return formatReadme(MysticSpellsReadme.title.html(), MysticSpellsReadme.tech0, MysticSpellsReadme.tech1, MysticSpellsReadme.insight.html(), MysticSpellsReadme.description.html())
+    }
+}
 
+const NodeSiriReadme = {
+    title: $("<span>").html("Node-Siri"),
+    tech0: [
+        ""
+    ],
+    tech1: [
+        ""
+    ],
+    insight: $("<span>").html(""),
+    description: $("<span>").html(""),
+    format: function() {
+        return formatReadme(NodeSiriReadme.title.html(), NodeSiriReadme.tech0, NodeSiriReadme.tech1, NodeSiriReadme.insight.html(), NodeSiriReadme.description.html())
+    }
+}
+
+const GiffyReadme = {
+    title: $("<span>").html("Giffy"),
+    tech0: [
+        ""
+    ],
+    tech1: [
+        ""
+    ],
+    insight: $("<span>").html(""),
+    description: $("<span>").html(""),
+    format: function() {
+        return formatReadme(GiffyReadme.title.html(), GiffyReadme.tech0, GiffyReadme.tech1, GiffyReadme.insight.html(), GiffyReadme.description.html())
+    }
+}
+
+function formatReadme(title, tech0, tech1, insight, description) {
+    const readmeTemplate = $("<div>").addClass("container");
     const row0 = $("<div>").addClass("row");
     const row1 = $("<div>").addClass("row");
     const row2 = $("<div>").addClass("row");
@@ -134,11 +183,6 @@ function formatReadme(title, tech0, tech1, insight, description) {
     return text;
 }
 
-
-
-
-
 $(document).on("click", ".close-button", function () {
-    console.log("testing");
     $("#modal-container").remove();
 })
